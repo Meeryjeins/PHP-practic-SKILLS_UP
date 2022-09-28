58a8c722c779e7983d1cae3bbfa9e7e1a4a64bd1
Jane M <jane.pfaeskillsup@gmail.com>
2022-09-28 10:43:11 +0100

    modificacion de ficheros , continuación del código new_reg.php

--- modules/include/new_reg.php
+++ modules/include/new_reg.php
@@ -8,8 +8,50 @@ $name = $email = $phone = $street = $city = $state = $zip = $other = $news = $ne
 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
 
-    if(!empty($_POST["name"]) || !empty($_POST["email"]) || !empty($_POST["phone"])){
+    if(!empty($_POST["name"]) || !empty($_POST["email"]) || !empty($_POST["phone"]))
     echo "<br><strong>name post hay datos</strong><br>";
+
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $phone = $_POST["phone"];
+    $street = $_POST["street"];
+    $city = $_POST["city"];
+    $state = $_POST["state"];
+    $zip = $_POST["zip"];
+    $other = $_POST["other"];
+    $news = $_POST["news"];
+    $newscheck = $_POST["newscheck"];
+
+    function limpiar_dato($data){
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+
+        }
+
+        //nombre , email y nº telefono.
+        function validar_name($name){
+            if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
+                return false;
+                }else{
+                    return true;
+                }
+        function validar_email($email){
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            return false;
+                }else{
+                    return true;
+                    }
+            }
+            function validar_phone($phone){
+                if(preg_match('/^[0-9]{10}+$/",$phone)){
+                    echo"valid phone number";
+                }else{
+                    echo m"invalid phone number";
+                }
+            }
+        }
     }
-}
+    
+
 ?>
